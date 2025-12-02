144c2ca7b7ab17ef9d86d6dc401e930b1855d5bc
daulat <daulatsingh7878@gmail.com>
2025-12-03 00:33:06 +0530

    implement binary tree

---
+++ BinaryTree/implementBinaryTree.cpp
@@ -0,0 +1,14 @@
+class Node{
+    public:
+    int data;
+    int *left, *right;
+    Node(int value){
+        this->data = value;
+        left=right= nullptr;
+    }
+};
+
+
+int main(){
+    
+}
