81d3828b7de5f24c8e89032ab93877b5818bc51c
daulat <daulatsingh7878@gmail.com>
2025-12-03 00:52:03 +0530

    implement binary tree

--- BinaryTree/implementBinaryTree.cpp
+++ BinaryTree/implementBinaryTree.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
 class Node{
     public:
     int data;
-    int *left, *right;
+    Node *left, *right;
     Node(int value){
         this->data = value;
         left=right= nullptr;
@@ -10,5 +14,25 @@ class Node{
 
 
 int main(){
-    
+    int x, first, second;
+    cin>>x;
+    queue<Node*> q;
+    Node* root = new Node(x);
+    q.push(root);
+    while(!q.empty()){
+        Node *current = q.front();
+        q.pop();
+        cout<<"Enter left child of "<<current->data<<": ";
+        cin>>first;
+        if (first != -1){
+            current->left = new Node(first);
+            q.push(current->left);
+        }
+        cout<<"Enter right child of "<<current->data<<": ";
+        cin>>second;
+        if (second != -1){
+            current->right = new Node(second);
+            q.push(current->right);
+        }
+    }
 }
