25e0fa0bc43b25b0a9b9d056b5d25f6b948c427f
daulat <daulatsingh7878@gmail.com>
2025-12-09 00:38:02 +0530

    add pre, post and inorder traversal

--- BinaryTree/implementBinaryTree2.cpp
+++ BinaryTree/implementBinaryTree2.cpp
@@ -20,12 +20,49 @@ Node* BinaryTree(){
     temp->left= BinaryTree();
     cout<<"Enter RIGHT child of "<<x<<endl;
     temp->right= BinaryTree();
-    return nullptr;
+    return temp;
 }
+
+
+void inOrder(Node *root){
+    if (root==NULL){
+        return;
+    };
+    
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+void postOrder(Node *root){
+    if (root==NULL) return;
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+void preOrder(Node *root){
+    if (root==NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+
 
 int main(){
     cout<<"Enter root value: "<<endl;
     Node* root;
     root= BinaryTree();
-    cout<<root;
+    // cout<<root;
+
+    inOrder(root);
+    cout<<endl;
+    postOrder(root);
+    cout<<endl;
+    preOrder(root);
 }
